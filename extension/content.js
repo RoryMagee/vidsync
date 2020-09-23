443dfd463fbc94852f8ac713ceb189ccc88fe626
Rory <rory.magee2@gmail.com>
2020-09-24 00:10:05 +0100

    added joinSession, leaveSession, getConnectionStatus and setConnectionStatus functions

--- extension/content.js
+++ extension/content.js
@@ -1,3 +1,6 @@
+let ws;
+let video;
+
 chrome.runtime.onMessage.addListener((request, sender, response) => {
     switch(request) {
         case 'join':
@@ -6,10 +9,71 @@ chrome.runtime.onMessage.addListener((request, sender, response) => {
             break;
         case 'leave':
             // Disconnect from websocket server
-            console.log(test());
+            leaveSession();
+            console.log('leaving session');
             break;
         default:
+            // Command not found
             console.log('request handler not found');
             break;
     }
 });
+
+//TODO: Add onmessage handler for websocket
+function joinSession() {
+    console.log('joining session');
+    if (!isSocketConnected()) {
+        // Socket is not connected so we can attempt to connect
+        ws = new WebSocket(websocketAddress());
+        setConnectionStatus(true);
+        getVideo();
+        console.log(video);
+        console.log(ws);
+        console.log('Connection successfully established');
+    }
+}
+
+function leaveSession() {
+    if (isSocketConnected()) {
+        // Socket is connected
+        ws.close();
+    }
+}
+
+function getVideo() {
+    video = document.querySelector('video[src]');
+    if (video) {
+        video.onplay = () => {
+            ws.send('play');
+        }
+        video.onpause = () => {
+            ws.send('pause');
+        }
+    } else {
+        console.error('Video not found in website');
+    }
+}
+
+function isSocketConnected() {
+    if (ws?.readyState === 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function setConnectionStatus(isConnected) {
+    chrome.storage.local.set({websocketConnected: isConnected});
+}
+
+function getConnectionStatus() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get('websocketConnected', (res) => {
+            if (res.hasOwnProperty('websocketConnected')) {
+                resolve(res['websocketConnected']);
+            } else {
+                reject('value not found');
+            }
+        });
+    });
+}
