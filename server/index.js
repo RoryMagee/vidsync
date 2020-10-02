cb3c814b0c213be6a57aa321b9d7e737c89469c1
Rory <rory.magee2@gmail.com>
2020-10-02 23:12:18 +0100

    updated server to deal with seek instruction

--- server/index.js
+++ server/index.js
@@ -22,6 +22,27 @@ wss.on('connection', (conn) => {
     console.log('client connected');
     console.log(`${wss.clients.size} clients connected`);
     conn.on('message', (message) => {
+        if (message !== 'keepalive' && client !== conn && client.readyState === Websocket.OPEN) {
+            console.log(message);
+            let parsed = JSON.parse(message);
+            switch(parsed['operation']) {
+                case 'playpause':
+                    client.send({
+                        operation: 'playpause',
+                        value: parsed['value']
+                    });
+                    break;
+                case 'seek':
+                    client.send({
+                        operation: 'seek',
+                        value: parsed['value']
+                    });
+                    break;
+                default:
+                    console.log(message);
+                    console.log('No handler found');
+            }
+        }
         if (message !== 'keepalive') {
             wss.clients.forEach(client => {
                 if (client !== conn && client.readyState === WebSocket.OPEN) {
