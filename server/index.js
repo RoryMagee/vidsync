105dfb2a3ecfdf51f7d33ef799367f4041266ae1
Rory <rory.magee2@gmail.com>
2020-10-05 21:58:06 +0100

    refactored server code

--- server/index.js
+++ server/index.js
@@ -9,44 +9,27 @@ app.get('/', (req, res) => {
     res.send("<h1>👀</h1>");
 });
 
+// Create server and listen on port 3000
 const httpServer = http.createServer(app);
 httpServer.listen(3000, () => {
     console.log('listening on port 3000');
 });
 
+// Create a websocket server from the HTTP server created above
 const wss = new ws.Server({
     server: httpServer
 });
 
+// Create handling for receiving websocket message
 wss.on('connection', (conn) => {
     console.log('client connected');
     console.log(`${wss.clients.size} clients connected`);
     conn.on('message', (message) => {
+        // If the message is a keepalive message then we can ignore it
         if (message !== 'keepalive') {
             wss.clients.forEach(client => {
                 if (client !== conn && client.readyState === ws.OPEN) {
-                    console.log('sending message to client\n', message);
-                    let parsed = JSON.parse(message);
-                    switch(parsed['operation']) {
-                        case 'playpause':
-                            client.send(JSON.stringify({
-                                operation: 'playpause',
-                                value: parsed['value']
-                            }));
-                            break;
-                        case 'seek':
-                            client.send(JSON.stringify({
-                                operation: 'seek',
-                                value: parsed['value']
-                            }));
-                            break;
-                        case 'disconnect':
-                            client.disconnect();
-                            break;
-                        default:
-                            console.log(message);
-                            console.log('No handler found');
-                    }
+                    websocketHandler(message, client);
                 } else {
                     console.log('not sending message to client\n', message);
                 }
@@ -54,3 +37,27 @@ wss.on('connection', (conn) => {
         }
     });
 });
+
+function websocketHandler(message, client) {
+    let parsed = JSON.parse(message);
+    switch(parsed['operation']) {
+        case 'playpause':
+            emitValue('playPause', parsed['value'], client);
+            break;
+        case 'seek':
+            emitValue('seek', parsed['value'], client);
+            break;
+        case 'disconnect':
+            client.disconnect();
+            break;
+        default:
+            console.log(`No handler found for ${message}`);
+    }
+}
+
+function emitValue(operation, value, client) {
+    client.send(JSON.stringify({
+        operation: operation,
+        value: value
+    }));
+}
